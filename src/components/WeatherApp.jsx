85a717ef9de8aa3e37cee535f843af17f16ca5d4
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-10-10 10:21:50 +1100

    feat: windspeed component added

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,10 +1,14 @@
+import React, { useState } from 'react';
 import SearchBar from "./SearchBar";
-
+import WindSpeed from "./WindSpeed";
 
 export default function WeatherApp() {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+  const [weatherData, setWeatherData] = useState(null);
+
+  return (
+    <div>
+      <SearchBar setWeatherData={setWeatherData} />
+      <WindSpeed windData={weatherData && weatherData.wind} />
+    </div>
+  )
 }
