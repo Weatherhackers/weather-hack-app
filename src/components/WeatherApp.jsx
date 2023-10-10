9b5a7e65f6cfc1a1f8018bfea45328aacbdc2bb9
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-10-10 14:01:06 +1100

    feat: weather description component added

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import SearchBar from "./SearchBar";
 import WindSpeed from "./WindSpeed";
+import WeatherDescription from './WeatherDescription';
 
 export default function WeatherApp() {
   const [weatherData, setWeatherData] = useState(null);
 
   return (
     <div>
-      <SearchBar setWeatherData={setWeatherData} />
-      <WindSpeed windData={weatherData && weatherData.wind} />
+      <div>
+        <SearchBar setWeatherData={setWeatherData} />
+      </div>
+      <div>
+        <WindSpeed windData={weatherData && weatherData.wind} />
+        <WeatherDescription weatherData={weatherData} />
+      </div>
     </div>
   )
 }
