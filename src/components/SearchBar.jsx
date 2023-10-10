674b9e637f07416626cb88a82e7ae1e40988190c
Helen T <helen-thai@hotmail.com>
2023-10-10 16:37:49 +1100

    feat: updated input fields into form to enable keyboard presses

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,7 +11,8 @@ export default function SearchBar({ setWeatherData }) {
   let apiKey = process.env.REACT_APP_API_KEY;
 
   // Search function
-  const search = async () => {
+  const search = async (event) => {
+    event.preventDefault();
     let element = document.getElementById("searchField");
     if (element.value === "") {
       return 0;
@@ -27,18 +28,20 @@ export default function SearchBar({ setWeatherData }) {
   }
 
   return (
-    <nav>
-      <input
+    <div>
+      <form onSubmit={search}>
+        <input
         type="text"
         name="searchField"
         id="searchField"
         placeholder="Search a city"
-      />
-      <div className="searchIconContainer" onClick={() => search()}>
-        <svg id="searchIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-        </svg>
-      </div>
-    </nav>
+        />
+        <button className="searchIconBtn">
+          <svg id="searchIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </button>
+      </form>
+    </div>
   )
 }
