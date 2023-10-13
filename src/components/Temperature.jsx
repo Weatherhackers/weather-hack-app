22905a08f8d0f4ada58736bae440e88dd352bb28
Stephen Gow <126635670+StevieG46@users.noreply.github.com>
2023-10-14 06:23:36 +1100

    feat: toggle temperature (#15)

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -1,17 +1,30 @@
+import React, { useState } from 'react';
 import './styling/Temperature.css';
 
 export default function Temperature({tempData}) {
+    const [isCelsius, setIsCelsius] = useState(true);
+
+    const toggleUnit = () => {
+      setIsCelsius((prevIsCelsius) => !prevIsCelsius);
+    };  
 
     // If data exists
     if (tempData) {
-        // rounding the temperature
-        let temp = Math.round(tempData.main.temp);
+        // Calculate the temperature in both Celsius and Fahrenheit
+        const celsiusTemp = Math.round(tempData.main.temp);
+        const fahrenheitTemp = Math.round((celsiusTemp * 9/5) + 32);
+
+    // Determine which temp to display based on the unit
+    const displayedTemp = isCelsius ? celsiusTemp : fahrenheitTemp;
+    const unit = isCelsius ? '°C' : '°F';
 
         // Return temperature
         return (
             <div>
-               <p className="temperature">{temp}&deg;C</p>
+                <p className="temperature"onClick={toggleUnit}>
+                    {displayedTemp}{unit}
+                </p>
             </div>
-        )
+        );
     }
 }
