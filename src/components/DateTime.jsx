155ef1aa0fd87ef049f27e63cf3cac05c583a841
Helen T <helen-thai@hotmail.com>
2023-10-13 23:12:34 +1100

    feat: updated date and time to match location timezone

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -1,10 +1,33 @@
 import './styling/DateTime.css';
 
-export default function DateTime() {
+export default function DateTime({weatherData}) {
     
     const date = new Date();
-    const currentDate = date.toLocaleDateString()
-    const currentTime = new Intl.DateTimeFormat('en-AU', {hour: 'numeric', minute: 'numeric'}).format(date).toUpperCase();
+
+    // Find the local time (default: milliseconds)
+    const localDateTime = date.getTime();
+    // console.log(`Local time is ${localDateTime}`)
+
+    // Find the offset - the difference between local time and UTC time
+    // (default: mins -> results: milliseconds)
+    const localDateTimeOffset = date.getTimezoneOffset() * (1000 * 60);
+    // console.log(`Time offset is ${localDateTimeOffset}`)
+
+    // Calculate UTC time
+    const utcTime = localDateTime + localDateTimeOffset;
+    // console.log(`UTC time is ${utcTime}`)
+
+    // Get weatherData timezone (default: seconds -> result: milliseconds)
+    const weatherDataTimezone = weatherData.timezone * 1000;
+    // console.log(`weatherData timezone is ${weatherDataTimezone}`)
+
+
+    // Calculate location time
+    const locationDateTime = new Date(utcTime + weatherDataTimezone);
+    // console.log(`locationDateTime is ${locationDateTime}`)
+
+    const currentDate = new Intl.DateTimeFormat(undefined, {day: 'numeric', month: 'short', year: 'numeric'}).format(locationDateTime);
+    const currentTime = new Intl.DateTimeFormat(undefined, {hour: 'numeric', hourCycle: 'h12', minute: 'numeric', timeZoneName: 'short'}).format(locationDateTime).toUpperCase();
 
     return (
         <div>
--- src/components/WindSpeed.jsx
+++ src/components/WindSpeed.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function WindSpeed({ windData }) {
-  console.log(windData); 
+  // console.log(windData); 
 
   if (windData && windData.speed) {
     return (
